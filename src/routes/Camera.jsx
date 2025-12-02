e8b669aa949c1f8283cc1c2808070d19dce81cdc
Manuel Basto <basto3107@gmail.com>
2025-12-02 08:30:46 -0600

    profile aspect

--- src/routes/Camera.jsx
+++ src/routes/Camera.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 
 export const Camera = () => {
     const videoRef = useRef(null);
-    const canvas = useRef(null);
+    const [profilePicture, setprofilePicture] = useState("");
     const useCamera = async () => {
         try {
             const stream = await navigator.mediaDevices.getUserMedia({
@@ -37,7 +37,8 @@ export const Camera = () => {
             const imgURL = URL.createObjectURL(blob);
             const imgElement = document.createElement("img");
             imgElement.src = imgURL;
-            document.body.appendChild(imgElement); // o usar un ref en React
+            document.body.appendChild(imgElement);
+            setprofilePicture(imgURL);
         } catch (error) {
             console.error("takePhoto() error:", error);
         }
@@ -45,19 +46,98 @@ export const Camera = () => {
 
     return (
         <>
-            <div className="p-2 d-flex gap-2 justify-content-center">
-                <button className="btn btn-primary" onClick={useCamera}>
-                    Video
+            <nav className="navbar bg-body-tertiary">
+                <div className="container d-flex justify-content-between">
+                    {profilePicture != "" ? (
+                        <img height={50} width={50} />
+                    ) : (
+                        <img
+                            src="images/placeholder/profile-holder.png"
+                            height={50}
+                            width={50}
+                        />
+                    )}
+
+                    <h5>Perfil</h5>
+                    <h3>ClotheStore</h3>
+                </div>
+            </nav>
+
+            <div className="p-2">
+                <button
+                    type="button"
+                    className="btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                >
+                    Tomar foto
                 </button>
-                <button className="btn btn-success" onClick={takePhoto}>
-                    Tomar captura
-                </button>
+                <div
+                    className="modal fade"
+                    id="exampleModal"
+                    tabIndex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1
+                                    className="modal-title fs-5"
+                                    id="exampleModalLabel"
+                                >
+                                    Modal title
+                                </h1>
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                ></button>
+                            </div>
+                            <div className="modal-body">
+                                <div className="p-2 d-flex gap-2 justify-content-center">
+                                    <button
+                                        className="btn btn-primary"
+                                        onClick={useCamera}
+                                    >
+                                        Video
+                                    </button>
+                                    <button
+                                        className="btn btn-success"
+                                        onClick={takePhoto}
+                                    >
+                                        Tomar captura
+                                    </button>
+                                </div>
+                                <div className="d-flex justify-content-center flex-column gap-2">
+                                    <video
+                                        className="bg-secondary"
+                                        autoPlay
+                                        ref={videoRef}
+                                    ></video>
+                                </div>
+                                <script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
+                            </div>
+                            <div className="modal-footer">
+                                <button
+                                    type="button"
+                                    className="btn btn-secondary"
+                                    data-bs-dismiss="modal"
+                                >
+                                    Close
+                                </button>
+                                <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                >
+                                    Save changes
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="d-flex justify-content-center flex-column gap-2">
-                <video className="bg-secondary" autoPlay ref={videoRef}></video>
-                <canvas className="bg-secondary" ref={canvas}></canvas>
-            </div>
-            <script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
         </>
     );
 };
